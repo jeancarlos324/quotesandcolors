6710f74cab27ec5e128018250138b62229a463e9
Jean Ticona <68865099+jeancarlos324@users.noreply.github.com>
2022-08-25 21:56:57 -0500

    Added card component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <Card/>
+    </div>
+  )
+}
+
+export default App
